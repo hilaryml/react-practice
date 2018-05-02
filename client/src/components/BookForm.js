e16243f22d37d91cbe86d90bc0a17428841e5e0b
hilaryml <hilyttle@gmail.com>
2018-05-02 14:17:29 -0400

    moving props argument to BookForm.js instead of App.js

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 export default class BookForm extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       title: '',
