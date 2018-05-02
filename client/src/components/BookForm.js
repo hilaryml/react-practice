6b13cd68dc43062608750fdb2c1eaf08d4d4687a
hilaryml <hilyttle@gmail.com>
2018-05-02 14:01:51 -0400

    adds event handlers

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -12,27 +12,72 @@ export default class BookForm extends Component {
     }
   }
 
+  handleOnTitleChange(event) {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  handleOnDescriptionChange(event) {
+    this.setState({
+      description: event.target.value
+    })
+  }
+
+  handleOnImageUrlChange(event) {
+    this.setState({
+      image_url: event.target.value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    return fetch('/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(response => response.json())
+    .then(book => console.log(book))
+    .catch(err => console.log(err))
+  }
+
   render() {
+    console.log(this.state)
     return (
-      <form>
-        <p>
-          <label>Title:</label>
-          <input type="text" />
-        </p>
-        <p>
-          <label>Description:</label>
-          <input type="text" />
-        </p>
-        <p>
-          <label>Image Url:</label>
-          <input type="text" />
-        </p>
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <p>
+            <label>Title:</label>
+            <input
+              type="text"
+              onChange={event => this.handleOnTitleChange(event)}
+              value={this.state.title}
+              placeholder="Book Title" />
+          </p>
+          <p>
+            <label>Description:</label>
+            <input type="text"
+            onChange={event => this.handleOnDescriptionChange(event)}
+            value={this.state.description}
+            placeholder="Book Description" />
+          </p>
+          <p>
+            <label>Image Url:</label>
+            <input type="text"
+            onChange={event => this.handleOnImageUrlChange(event)}
+            value={this.state.image_url}
+            placeholder="Image Url" />
+          </p>
 
-        <input
-          type="submit"
-          value="Add Book" />
+          <input
+            type="submit"
+            value="Add Book" />
 
-      </form>
+        </form>
+      </div>
     )
   }
 }
