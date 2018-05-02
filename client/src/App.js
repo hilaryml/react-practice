6741be861a11615d6c699abe0fa97266c49f8c13
hilaryml <hilyttle@gmail.com>
2018-05-02 11:51:14 -0400

    adding componentDidMount and constructor

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/books', {
+      method: 'GET',
+      headers: {
+        'Accepts': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(books => this.setState({ books }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    console.log(this.state.books);
     return (
       <div className="App">
         <header className="App-header">
